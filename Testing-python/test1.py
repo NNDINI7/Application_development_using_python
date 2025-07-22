af9a054525aa568fa20fa10c298e6fd9aa095cf5
Nandini Giri <118757788+NNDINI7@users.noreply.github.com>
2025-07-22 19:27:23 +0530

    Add files via upload

---
+++ Testing-python/test1.py
@@ -0,0 +1,6 @@
+# assert : je pn statement lihu te check krte whether it is true or false if it is true then ok if false it provide a msg to solve that error.
+# testing is imp to check whether our application works properly as expected or not .
+
+# there are two types of test :
+# 1. unit test 
+# 2. py test 
